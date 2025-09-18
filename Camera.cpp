8e0db67fdb201fc44e0e309ae4ed58bae04ad7f8
Elliot <96925029+elliotputnam@users.noreply.github.com>
2025-09-18 10:54:55 -0700

    Adding movement

--- Camera.cpp
+++ Camera.cpp
@@ -14,9 +14,25 @@ Camera::Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLf
 	Update();
 }
 
+void Camera::KeyControl(bool* keys)
+{
+	if (keys[GLFW_KEY_W]) { position += front * moveSpeed; }
+	if (keys[GLFW_KEY_A]) { position -= right * moveSpeed; }
+	if (keys[GLFW_KEY_S]) { position -= front * moveSpeed; }
+	if (keys[GLFW_KEY_D]) { position += right * moveSpeed; }
+}
+
 void Camera::Update()
 {
-	 
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front = glm::normalize(front);
+
+	// calc right using front and world so it's level to ground
+	right = glm::normalize(glm::cross(front, worldUp));
+	//calc up using right and front
+	up = glm::normalize(glm::cross(right, front));
 }
 
 Camera::~Camera()
--- Camera.h
+++ Camera.h
@@ -5,12 +5,17 @@
 #include <glm\glm.hpp>
 #include <glm\gtc\matrix_transform.hpp>
 
+#include <GLFW\glfw3.h>
+
 
 class Camera
 {
 public:
 
 	Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed);
+
+	void KeyControl(bool* keys);
+
 	~Camera();
 
 private:
