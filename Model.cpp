f74486678056987ec6ae2da984cff3a10bff17cb
Elliot <96925029+elliotputnam@users.noreply.github.com>
2025-10-10 16:19:59 -0400

    Model class created
    needs debugging

---
+++ Model.cpp
@@ -0,0 +1,152 @@
+#include "Model.h"
+
+void Model::LoadModel(const std::string& fileName)
+{
+
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(fileName, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenSmoothNormals | aiProcess_JoinIdenticalVertices);
+
+	if (!scene)
+	{
+		printf("Model (%s) has failed to load: %s", fileName, importer.GetErrorString());
+		return;
+	}
+
+	LoadNode(scene->mRootNode, scene);
+
+	LoadMaterials(scene);
+}
+
+void Model::ClearModel()
+{
+	for (size_t i = 0; i < meshList.size(); i++)
+	{
+		if (meshList[i])
+		{
+			delete meshList[i];
+			meshList[i] = nullptr;
+		}
+	}
+	for (size_t i = 0; i < textureList.size(); i++)
+	{
+		if (textureList[i])
+		{
+			delete textureList[i];
+			textureList[i] = nullptr;
+		}
+	}
+}
+
+void Model::RenderModel()
+{
+	for (size_t i = 0; i < meshList.size(); i++)
+	{
+		unsigned int materialIndex = meshToTex[i];
+
+		if (materialIndex < textureList.size() && textureList[materialIndex])
+		{
+			textureList[materialIndex]->UseTexture();
+		}
+		meshList[i]->RenderMesh();
+	}
+}
+
+Model::~Model()
+{
+}
+
+void Model::LoadNode(aiNode* node, const aiScene* scene)
+{
+	for (size_t i = 0; i < node->mNumMeshes; i++)
+	{
+		// node->mMeshes[i] holds the ID of the mesh
+		LoadMesh(scene->mMeshes[node->mMeshes[i]], scene);
+	}
+
+	for (size_t i = 0; i < node->mNumChildren; i++)
+	{
+		LoadNode(node->mChildren[i], scene);
+	}
+}
+
+void Model::LoadMesh(aiMesh* mesh, const aiScene* scene)
+{
+	std::vector<GLfloat> vertices;
+	std::vector<unsigned int> indices;
+
+	for (size_t i = 0; i < mesh->mNumVertices; i++)
+	{
+		// insert vertices
+		vertices.insert(vertices.end(), { mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z });
+		// check if texture exists for model
+		if (mesh->mTextureCoords[0])
+		{
+			// add texture UV
+			vertices.insert(vertices.end(), { mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y });
+		}
+		else
+		{
+			// pass defaults
+			vertices.insert(vertices.end(), { 0.0f, 0.0f });
+		}
+		// insert reverse normals here if shader does not invert
+		vertices.insert(vertices.end(), { -mesh->mNormals[i].x, -mesh->mNormals[i].y, -mesh->mNormals[i].z });
+	}
+
+	// each face
+	for (size_t i = 0; i < mesh->mNumFaces; i++)
+	{
+		// go through each point it contains
+		aiFace face = mesh->mFaces[i];
+		for (size_t j = 0; j < face.mNumIndices; j++)
+		{
+			// add to indices list
+			indices.push_back(face.mIndices[j]);
+		}
+	}
+
+	Mesh* newMesh = new Mesh();
+	newMesh->CreateMesh(&vertices[0], &indices[0], vertices.size(), indices.size());
+	meshList.push_back(newMesh);
+
+	meshToTex.push_back(mesh->mMaterialIndex);
+}
+
+void Model::LoadMaterials(const aiScene* scene)
+{
+	// creates many textures the same size as materials
+	textureList.resize(scene->mNumMaterials);
+
+	for (size_t i = 0; i < scene->mNumMaterials; i++)
+	{
+		aiMaterial* material = scene->mMaterials[i];
+
+		textureList[i] = nullptr;
+
+		if (material->GetTextureCount(aiTextureType_DIFFUSE))
+		{
+			aiString path;
+			if (material->GetTexture(aiTextureType_DIFFUSE, 0, &path) == AI_SUCCESS)
+			{
+				int idx = std::string(path.data).rfind("\\");
+				std::string filename = std::string(path.data).substr(idx + 1);
+
+				std::string texPath = std::string("Textures/") + filename;
+
+				textureList[i] = new Texture(texPath.c_str());
+
+				if (!textureList[i]->LoadTexture())
+				{
+					printf("Failed to load texture at: %s", texPath);
+					delete textureList[i];
+					textureList[i] = nullptr;
+				}
+			}
+		}
+		if (!textureList[i])
+		{
+			textureList[i] = new Texture("Textures/plain.png");
+			textureList[i]->LoadTextureA();
+		}
+	}
+}
--- OpenGLApp.cpp
+++ OpenGLApp.cpp
@@ -24,10 +24,13 @@
 #include "DirectionalLight.h"
 #include "PointLight.h"
 #include "SpotLight.h"
+#include "Model.h"
 
 #include "assimp/Importer.hpp"
 #include <assimp/BaseImporter.h>
 #include <assimp/Importer.hpp>
+
+
 
 const float toRadians = 3.14159265f / 180.0f;
 
@@ -44,6 +47,8 @@ Texture plainTexture;
 // MATERIALS
 Material shinyMaterial;
 Material dullMaterial;
+
+Model heli;
 
 // LIGHTS
 DirectionalLight mainLight;
@@ -225,6 +230,9 @@ int main()
 	// Specular lighting
 	shinyMaterial = Material(1.0f, 32);
 	dullMaterial = Material(0.3f, 4);
+
+	heli = Model();
+	heli.LoadModel("Models/Seahawk.obj");
 	
 	// load lighting
 	mainLight = DirectionalLight
@@ -463,6 +471,14 @@ int main()
 		dullMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		brickTexture.UseTexture();
 		meshList[8]->RenderMesh();
+
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 2.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		dullMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		heli.RenderModel(); 
 
 		glUseProgram(0);
 
